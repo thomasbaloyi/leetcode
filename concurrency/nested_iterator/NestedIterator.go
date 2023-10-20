d8d4f3c685271600dd4f19b5cf63a650d2978f96
Thomas Baloyi <thomas.baloyi@pathcare.net>
2023-10-20 15:34:36 +0200

    Park - hard!

--- concurrency/nested_iterator/NestedIterator.go
+++ concurrency/nested_iterator/NestedIterator.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ *  Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (this NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (this NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (this *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (this NestedInteger) GetList() []*NestedInteger {}
+ */
 
-func main() {
-	fmt.Println("Hello world")
+type NestedInteger struct {
+}
+
+type NestedIterator struct {
+	NestedList []*NestedInteger
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	n := new(NestedIterator)
+	n.NestedList = append(nestedList)
+	return n
+}
+
+func (this *NestedIterator) Next() int {
+	for i := 0; i < len(this.NestedList); i++ {
+		return 1
+	}
+}
+
+func (this *NestedIterator) HasNext() bool {
+	return true
 }
